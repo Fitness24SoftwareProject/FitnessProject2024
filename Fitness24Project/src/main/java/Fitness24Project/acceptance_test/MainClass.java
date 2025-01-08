419df2409fa012c1584621f8c6f753944c909582
Beesan Jian <s12143418@stu.najah.edu>
2025-01-08 21:05:11 +0200

    Update MainClass.java

--- Fitness24Project/src/main/java/Fitness24Project/acceptance_test/MainClass.java
+++ Fitness24Project/src/main/java/Fitness24Project/acceptance_test/MainClass.java
@@ -1,6 +1,7 @@
 package Fitness24Project.acceptance_test;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class MainClass {
    private static final Logger logger = Logger.getLogger(MainClass.class.getName());
