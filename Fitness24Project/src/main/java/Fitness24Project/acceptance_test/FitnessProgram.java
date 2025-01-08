bab2b2f9c5f5386d1e47c595f7e99996fcde2553
USER-M <USER-M@BEESANS-LAPTOP>
2025-01-08 14:27:52 +0200

    First Commit.

--- Fitness24Project/src/main/java/Fitness24Project/acceptance_test/FitnessProgram.java
+++ Fitness24Project/src/main/java/Fitness24Project/acceptance_test/FitnessProgram.java
@@ -6,6 +6,7 @@ public class FitnessProgram {
     private String difficulty;
     private String goals;
     private double price;
+    int yes;
 
     // Constructor
     public FitnessProgram(String title, int duration2, String difficulty, String goals, double price) {
